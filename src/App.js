61d715e6329e1b65290316845a879e21a89a23bf
Benny Luo <bennyluo95@gmail.com>
2021-09-08 15:54:55 -0400

    initial save

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react'
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 const Menu = () => {
   const padding = {
@@ -6,9 +11,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/anecdotes">anecdotes</Link>
+      <Link style={padding} to="/create">create</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root'))
